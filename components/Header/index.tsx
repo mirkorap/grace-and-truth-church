00960259d0c358c89a42d7a20ad1cf8e882c7a4e
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-07-07 21:50:35 +0200

    feat: add Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import Navbar from '@/components/Navbar';
+
+export default function Header() {
+  return (
+    <header className='absolute flex w-full flex-wrap bg-scaffold py-4 text-sm sm:flex-nowrap sm:justify-start'>
+      <Navbar />
+    </header>
+  );
+}
---
+++ components/Navbar/constants.ts
@@ -0,0 +1,34 @@
+import { NavItem } from '@/types/Navbar';
+
+export const NavItems: NavItem[] = [
+  {
+    id: 1,
+    href: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    href: '/chi-siamo',
+    text: 'Chi siamo',
+  },
+  {
+    id: 3,
+    href: '/incontri',
+    text: 'Incontri',
+  },
+  {
+    id: 4,
+    href: '/sermoni',
+    text: 'Sermoni',
+  },
+  {
+    id: 5,
+    href: '/eventi',
+    text: 'Ultime novità',
+  },
+  {
+    id: 6,
+    href: '/contattaci',
+    text: 'Contattaci',
+  },
+];
