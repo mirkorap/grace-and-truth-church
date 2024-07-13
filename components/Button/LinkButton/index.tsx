7eb6a65e7c8292f92c0e3aaa69280e643e124a41
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-07-13 22:25:11 +0200

    feat: add Button components

---
+++ components/Button/LinkButton/index.tsx
@@ -0,0 +1,21 @@
+import { Layouts } from '@/components/Button/constants';
+import { LinkButton as Button } from '@/types/Button';
+import Link from 'next/link';
+
+export default function LinkButton({
+  text,
+  href,
+  type,
+  style,
+  className,
+}: Button) {
+  return (
+    <Link
+      href={href}
+      type={type}
+      className={`inline-flex items-center gap-x-2 rounded border px-4 py-3 text-sm font-semibold disabled:pointer-events-none disabled:opacity-50 sm:px-8 sm:py-4 ${Layouts[style]} ${className}`}
+    >
+      {text}
+    </Link>
+  );
+}
---
+++ components/Button/constants.ts
@@ -0,0 +1,9 @@
+import { ButtonLayout } from '@/types/Button';
+
+export const Layouts: ButtonLayout = {
+  contained:
+    'border-transparent bg-primary-500 text-white hover:bg-primary-600',
+  outlined:
+    'border-primary-500 text-primary-500 hover:bg-primary-500 hover:text-white',
+  text: 'border-transparent text-primary-500 hover:text-primary-600',
+};
