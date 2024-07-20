4f8a7290e514cfbe27aababa4ab34b5958f1a84b
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-07-20 10:33:16 +0200

    feat: add Tailwind classes for mobile devices

--- components/Heading/HeadlineSmall/index.tsx
+++ components/Heading/HeadlineSmall/index.tsx
@@ -2,7 +2,9 @@ import { Heading } from '@/types/Heading';
 
 export default function HeadlineSmall({ text, className }: Heading) {
   return (
-    <h3 className={`text-2xl font-bold text-headline-700 ${className}`}>
+    <h3
+      className={`text-xl lg:text-2xl font-bold text-headline-700 ${className}`}
+    >
       {text}
     </h3>
   );
--- components/Heading/TitleLarge/index.tsx
+++ components/Heading/TitleLarge/index.tsx
@@ -2,7 +2,9 @@ import { Heading } from '@/types/Heading';
 
 export default function TitleLarge({ text, className }: Heading) {
   return (
-    <h4 className={`text-xl font-semibold text-headline-700 ${className}`}>
+    <h4
+      className={`text-lg lg:text-xl font-semibold text-headline-700 ${className}`}
+    >
       {text}
     </h4>
   );
