1f40d75557e1b5e2b6bd26eb29e801eb0a5d41e2
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-07-20 21:28:35 +0200

    feat: add ButtonSize

--- components/Button/LinkButton/index.tsx
+++ components/Button/LinkButton/index.tsx
@@ -7,13 +7,14 @@ export default function LinkButton({
   href,
   type,
   style,
+  size,
   className,
 }: Options) {
   return (
     <Link
       href={href}
       type={type}
-      className={`inline-flex items-center gap-x-2 rounded border px-4 py-3 text-sm font-semibold disabled:pointer-events-none disabled:opacity-50 sm:px-8 sm:py-4 ${Layouts[style]} ${className}`}
+      className={`inline-flex items-center gap-x-2 rounded border text-sm font-semibold disabled:pointer-events-none disabled:opacity-50 ${Layouts[style]} ${Layouts[size]} ${className}`}
     >
       {text}
     </Link>
