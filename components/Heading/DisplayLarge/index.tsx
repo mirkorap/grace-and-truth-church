192d046e356a331dbd3a0243f707e7ff9d33c53e
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-07-21 16:52:40 +0200

    feat: add Display headings

---
+++ components/Heading/DisplayLarge/index.tsx
@@ -0,0 +1,11 @@
+import { Heading as Options } from '@/types/Heading';
+
+export default function DisplayLarge({ text, className }: Options) {
+  return (
+    <h1
+      className={`text-6xl font-bold text-headline-700 lg:text-7xl ${className}`}
+    >
+      {text}
+    </h1>
+  );
+}
