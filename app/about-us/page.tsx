1bc95aa8d1d0773e196c3f7a06850e3afec1c805
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-05 16:52:07 +0200

    refactor: rename import DoctrinalLines into DoctrinalLinesSection

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,5 @@
 import Divider from '@/components/Divider';
-import DoctrinalLines from '@/containers/about-us-page/doctrinal-lines-section';
+import DoctrinalLinesSection from '@/containers/about-us-page/doctrinal-lines-section';
 import HeroSection from '@/containers/about-us-page/hero-section';
 
 export default function AboutUs() {
@@ -7,7 +7,7 @@ export default function AboutUs() {
     <main>
       <HeroSection />
       <Divider className='mx-auto max-w-60 md:max-w-[30rem]' />
-      <DoctrinalLines />
+      <DoctrinalLinesSection />
     </main>
   );
 }
