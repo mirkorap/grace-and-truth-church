11a9458766d5aa64ef443e8eef9dba645e2045f8
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-05 16:46:29 +0200

    feat: add DoctrinalLines section

---
+++ containers/about-us-page/doctrinal-lines-section/index.tsx
@@ -0,0 +1,30 @@
+import HeadlineMedium from '@/components/Heading/HeadlineMedium';
+
+import { DoctrinalItems } from './constants';
+import DoctrinalItem from './doctrinal-item';
+
+export default function DoctrinalLinesSection() {
+  return (
+    <section id='doctrinal-lines' className='w-full'>
+      <div className='mx-auto flex max-w-[85rem] flex-col items-center px-6'>
+        <HeadlineMedium
+          text='Lineamenti Dottrinali'
+          className='mb-20 text-primary-500'
+        />
+
+        <ul className='list-decimal'>
+          {DoctrinalItems.map((item) => {
+            return (
+              <DoctrinalItem
+                key={item.id}
+                title={item.title}
+                text={item.text}
+                verses={item.verses}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+}
