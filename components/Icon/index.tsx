11d131d6198502d7a6240083472a5d01939d62f8
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-07 14:54:10 +0200

    feat: add Icon component

---
+++ components/Icon/index.tsx
@@ -0,0 +1,7 @@
+import { Icon as Options } from '@/types/Icon';
+
+import { Layouts } from './constants';
+
+export default function Icon({ name, size, className }: Options) {
+  return <i className={`icon-${name} ${Layouts[size]} ${className}`}></i>;
+}
