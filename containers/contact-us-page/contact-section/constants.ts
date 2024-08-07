d8cd73beb33524903125ed04b5387a07bc340a37
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-07 15:30:09 +0200

    feat: add ContactItem type

---
+++ containers/contact-us-page/contact-section/constants.ts
@@ -0,0 +1,19 @@
+import { ContactItem } from './types';
+
+export const ContactItems: ContactItem[] = [
+  {
+    id: 1,
+    icon: 'pin',
+    text: 'Via Giuseppe Garibaldi 453 - 95029 Viagrande (CT)',
+  },
+  {
+    id: 2,
+    icon: 'phone',
+    text: '+39 329 71 17 849',
+  },
+  {
+    id: 3,
+    icon: 'mail',
+    text: 'chiesagraziaeverita@gmail.com',
+  },
+];
