b658ea7f10f5b3b633e99f93709b0ea7a916a081
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-07 15:54:14 +0200

    fix: replace bg-white with bg-card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -5,7 +5,7 @@ import CardImage from './CardImage';
 
 export default function Card({ title, text, href, imgSrc, imgAlt }: Options) {
   return (
-    <div className='group flex flex-col overflow-hidden rounded-xl border bg-white shadow-sm transition hover:shadow-lg'>
+    <div className='group flex flex-col overflow-hidden rounded-xl border bg-card shadow-sm transition hover:shadow-lg'>
       <CardImage href={href} imgSrc={imgSrc} imgAlt={imgAlt} />
       <CardBody title={title} text={text} href={href} />
     </div>
