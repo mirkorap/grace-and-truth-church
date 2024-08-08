aac0a5e7fffb84bc6e3e62653868e5c0dc537506
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-08-08 08:21:33 +0200

    feat: add Form type

---
+++ types/Form.ts
@@ -0,0 +1,15 @@
+export interface Input {
+  id: string;
+  name: string;
+  label: string;
+  placeholder: string;
+}
+
+export interface Textarea {
+  id: string;
+  name: string;
+  label: string;
+  placeholder: string;
+  cols: number;
+  rows: number;
+}
