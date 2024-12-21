1db5e7e424ee5aa51258486cc6c01659e15649a5
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-21 10:59:25 +0100

    refactor: move contact-us api to api folder

--- app/api/contact-us/route.ts
+++ app/api/contact-us/route.ts
@@ -3,9 +3,9 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request) {
+export async function POST(req: Request) {
   try {
-    const formData = await request.formData();
+    const formData = await req.formData();
 
     const formDataEntries = {
       name: formData.get('name')?.toString() ?? '',
