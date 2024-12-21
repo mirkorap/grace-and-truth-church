07e23d74e7eed798abcea9677c4044bfde81f6df
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-21 17:11:16 +0100

    feat: add sermons page

--- app/sermons/page.tsx
+++ app/sermons/page.tsx
@@ -1,3 +1,13 @@
+import HeroSection from '@/containers/sermons-page/hero-section';
+import SermonListSection from '@/containers/sermons-page/sermon-list-section';
+
 export default function Sermons() {
-  return <h1>Sermoni</h1>;
+  console.log('hello');
+
+  return (
+    <main>
+      <HeroSection />
+      <SermonListSection />
+    </main>
+  );
 }
