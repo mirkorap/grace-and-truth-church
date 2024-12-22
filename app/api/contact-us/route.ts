70a4f6a5bdb045d76262ffe3ca4c92289b019fb5
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-22 22:12:43 +0100

    fix: use NextRequest

--- app/api/contact-us/route.ts
+++ app/api/contact-us/route.ts
@@ -1,10 +1,10 @@
 import EmailTemplate from '@/containers/contact-us-page/contact-section/email-template';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
 
