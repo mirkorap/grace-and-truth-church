f6d36cd732a7aa1a7a14ad4028946c7e7459cdf2
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-22 21:35:35 +0100

    fix: add getLatestSermons async function

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
 import HeroSection from '@/containers/home-page/hero-section';
 import LastSermonsSection from '@/containers/home-page/last-sermons-section';
 import OurMeetingsSection from '@/containers/home-page/our-meetings-section';
-import { getLatestSermons as endpoint } from '@/libs/endpoints';
-import { typedFetch } from '@/libs/utils';
-import { Sermon } from '@/types/Sermon';
-import { GetStaticProps } from 'next';
 
-interface Props {
-  lastSermon: Sermon;
-  latestSermons: Sermon[];
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const [lastSermon, ...latestSermons] = await typedFetch<Sermon[]>(endpoint);
-
-  return {
-    props: { lastSermon, latestSermons },
-    revalidate: 3600,
-  };
-};
-
-export default function Home({ lastSermon, latestSermons }: Props) {
+export default function Home() {
   return (
     <main>
       <HeroSection />
       <OurMeetingsSection />
-      <LastSermonsSection
-        lastSermon={lastSermon}
-        latestSermons={latestSermons}
-      />
+      <LastSermonsSection />
     </main>
   );
 }
