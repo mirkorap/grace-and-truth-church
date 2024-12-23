9ca415c6afdf662c6de65e137631ffa10c2eebed
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-23 14:59:29 +0100

    build: install and configure plaiceholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPlaiceholder from '@plaiceholder/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -8,4 +10,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
