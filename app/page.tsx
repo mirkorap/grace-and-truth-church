dec26e7b50498bcee8d080c625f95cd4a8c60343
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-23 12:01:20 +0100

    fix: revert changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,13 @@
 import HeroSection from '@/containers/home-page/hero-section';
 import LastSermonsSection from '@/containers/home-page/last-sermons-section';
 import OurMeetingsSection from '@/containers/home-page/our-meetings-section';
-import { getLatestSermons as endpoint } from '@/libs/endpoints';
-import { typedFetch } from '@/libs/utils';
-import { Sermon } from '@/types/Sermon';
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-interface Props {
-  lastSermon: Sermon;
-  latestSermons: Sermon[];
-}
-
-export const getStaticProps = (async () => {
-  const [lastSermon, ...latestSermons] = await typedFetch<Sermon[]>(endpoint, {
-    next: { revalidate: 3600 },
-  });
-
-  return {
-    props: {
-      repo: { lastSermon, latestSermons },
-    },
-  };
-}) satisfies GetStaticProps<{
-  repo: Props;
-}>;
-
-export default function Home({
-  repo,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <main>
       <HeroSection />
       <OurMeetingsSection />
-      <LastSermonsSection
-        lastSermon={repo.lastSermon}
-        latestSermons={repo.latestSermons}
-      />
+      <LastSermonsSection />
     </main>
   );
 }
