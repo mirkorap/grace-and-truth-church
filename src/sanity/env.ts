24990155d244a6509005411e915a63be201e94f5
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-23 11:24:50 +0100

    fix: use SANITY_API_TOKEN

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,3 +1,3 @@
 export const projectId = process.env.SANITY_API_PROJECT_ID!;
 export const dataset = process.env.SANITY_API_DATASET!;
-export const token = process.env.SANITY_API_WRITE_TOKEN!;
+export const token = process.env.SANITY_API_TOKEN!;
