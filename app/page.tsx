4e8c7a766a6a90ab4e07fccb9bf5cb463e6458f0
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-23 11:21:22 +0100

    fix: use SANITY_API_WRITE_TOKEN

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import HeroSection from '@/containers/home-page/hero-section';
 import LastSermonsSection from '@/containers/home-page/last-sermons-section';
 import OurMeetingsSection from '@/containers/home-page/our-meetings-section';
 
-export const dynamic = 'force-dynamic';
-
 export default function Home() {
   return (
     <main>
