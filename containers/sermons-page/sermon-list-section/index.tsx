c13b3959f71a57c94ce9f93fcc056f161c3fda13
mirkorap16 <m.rapisarda@acadevmy.it>
2024-12-28 10:55:06 +0100

    fix: adjust padding

--- containers/sermons-page/sermon-list-section/index.tsx
+++ containers/sermons-page/sermon-list-section/index.tsx
@@ -47,7 +47,7 @@ export default async function SermonListSection() {
   const books = await fetchOnlyBooksUsedInSermons();
 
   return (
-    <section className='w-full py-32' id='sermon-list'>
+    <section className='w-full py-8' id='sermon-list'>
       <div className='flex flex-col items-start gap-y-10 px-4 py-12 lg:flex-row lg:gap-x-24 lg:gap-y-0'>
         <div className='order-last grid grid-cols-1 gap-8 lg:order-first lg:grid-cols-2'>
           {sermons.map((sermon) => (
