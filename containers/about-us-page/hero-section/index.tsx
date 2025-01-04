6bb0453ed6489048ba49fbe649b1796ea5c13336
Mirko Rapisarda <mirkorap16@gmail.com>
2025-01-04 13:37:49 +0100

    fix: adjust margins for all pages (#20)
    Co-authored-by: mirkorap16 <m.rapisarda@acadevmy.it>

--- containers/about-us-page/hero-section/index.tsx
+++ containers/about-us-page/hero-section/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section className='w-full px-4 pt-44' id='hero'>
+    <section className='w-full pt-44' id='hero'>
       <div className='flex flex-col gap-y-2 text-center'>
         <HeadlineLarge text='Chiesa Cristiana Evangelica Riformata' />
         <HeadlineLarge className='mb-10' text='"Grazia e Verità"' />
@@ -20,7 +20,7 @@ export default function HeroSection() {
         />
 
         <BodyLarge
-          className='mx-auto max-w-[40rem] text-center !text-base md:!text-xl'
+          className='max-w-[40rem] text-center !text-base md:!text-xl'
           text='Siamo una chiesa evangelica nata nel 2015, che vuole testimoniare che la grazia e la verità di Dio sono venute per mezzo di Gesù Cristo, il Figlio di Dio, il Salvatore.'
         />
       </div>
--- containers/contact-us-page/contact-section/index.tsx
+++ containers/contact-us-page/contact-section/index.tsx
@@ -4,7 +4,7 @@ import ContactInfo from './contact-info';
 export default function ContactSection() {
   return (
     <section className='mb-32 w-full pt-44' id='contact'>
-      <div className='mx-auto flex max-w-[85rem] flex-col gap-10 px-4 lg:flex-row lg:justify-between'>
+      <div className='flex flex-col gap-10 lg:flex-row lg:justify-between'>
         <ContactForm />
         <ContactInfo />
       </div>
